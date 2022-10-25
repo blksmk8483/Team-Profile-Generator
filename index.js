5e80d61db8ffbe8d8ea7a91187145e7c2cfab703
Brandon Knight <knightbrandon1984@gmail.com>
2022-10-25 13:52:26 -0500

    added most of my file structure, input for my classes

---
+++ index.js
@@ -0,0 +1,40 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is your Employee ID?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+    {
+      type: 'input',
+      name: 'number',
+      message: 'What is your office number?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+  ])
+
+// STill to do...
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Success')
+    );
+  });
